1ab1996ed7c63e9aba19fbdb91672e10380ea446
bhavika <bhavikagoyal08@gmail.com>
2026-06-29 00:45:58 +0530

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -8,15 +8,15 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
 
-    name = Column(String, nullable=False)
+    name = Column(String(100), nullable=False)
 
     email = Column(
-        String,
+        String(255),
         unique=True,
         index=True,
         nullable=False
     )
 
-    password = Column(String, nullable=False)
+    password = Column(String(255), nullable=False)
 
-    role = Column(String, default="student")
+    role = Column(String(50), default="student")
